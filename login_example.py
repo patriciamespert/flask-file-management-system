12fa04f5cc1cf6820aaf2926d92547b89d8f9d5a
Patricia Martinez Espert <pmespert@gmail.com>
2021-06-13 12:46:08 +0200

    initial flask login module steps

--- login_example.py
+++ login_example.py
@@ -1,6 +1,7 @@
 from flask import Flask
+from flask_login.utils import login_required
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin, login_manager
+from flask_login import LoginManager, UserMixin, login_user, current_user
 
 app = Flask(__name__)
 
@@ -17,3 +18,29 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(30), unique=True)
 
 db.create_all()
+
+#login_manager
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+#routes
+@app.route('/')
+def index():
+    user = User.query.filter_by(username='patricia').first()
+    login_user(user)
+    return 'You are now logged in!'
+
+@app.route('/logout')
+@login_required
+def logout():
+    return 'You are now logged out!'
+
+@app.route('/home')
+@login_required
+def home():
+    return 'The current user is {}'.format(current_user.username)
+
+if __name__ == '__main__':
+    app.run(debug=True)
